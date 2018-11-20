cd8723cf3e86d38c54a22b93304db9372b99c58e
U-WIT\luih <luih@WIT45000398.wit.private>
2018-11-20 17:30:07 -0500

    Created client for react front-end

---
+++ client/public/build/precache-manifest.ab5bdeb366b4cc07014f06e35a312047.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2e2c1aac043e8a02110",
+    "url": "/static/js/main.c2e2c1aa.chunk.js"
+  },
+  {
+    "revision": "916e4ad532338820642d",
+    "url": "/static/js/1.916e4ad5.chunk.js"
+  },
+  {
+    "revision": "c2e2c1aac043e8a02110",
+    "url": "/static/css/main.312766a5.chunk.css"
+  },
+  {
+    "revision": "46d6dfeb28bb2a2083ec55e267727fae",
+    "url": "/index.html"
+  }
+];
